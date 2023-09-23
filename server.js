6d1689c5aad8a4e31fec9e9d5c07ec73d5f5740b
boburtojiboev <tbobur1995@gmail.com>
2023-09-23 10:31:05 +0900

    feat: ejs framework orqali ananaviy Frontend qirish

--- server.js
+++ server.js
@@ -16,12 +16,15 @@ app.set("view engine", "ejs");
 
 
 // 4 Routing code
-app.get("/hello", function (req, res){
-    res.end(`<h1>HELLO WORLD</h1>`);
+app.post('/create-item', (req, res) => {
+    console.log(req.body);
+    res.json({test: "success" });
 });
-app.get("/gift", function (req, res){
-    res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
+
+app.get('/', function(req, res) {
+    res.render("harid");
 });
+
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function (){
