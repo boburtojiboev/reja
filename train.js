c8fc4c3914b879e6fdaef62deb84cbb4e0fed2c6
boburtojiboev <tbobur1995@gmail.com>
2023-09-26 10:16:37 +0900

    feat train callback hamda async functionlar

--- train.js
+++ train.js
@@ -1,4 +1,5 @@
-// Nodejs event loop va Callback functionlarni organamiz
+// Async functionlarni qo'llash
+
 console.log("Jack Ma maslahatlari");
 const list = [
     "yaxshi talaba boling", //0=20
@@ -9,6 +10,9 @@ const list = [
     "endi dam oling, foydasi yoq" //60
 ];
 
+
+// Nodejs event loop va Callback functionlarni organamiz
+
 function maslahatBering (a, callback) {
     if (typeof a !== 'number') callback("insert a number", null);
     else if (a <= 20) callback(null, list[0]);
@@ -17,10 +21,10 @@ function maslahatBering (a, callback) {
     else if (a > 40 && a <= 50) callback(null, list[3]);
     else if (a > 50 && a <= 60) callback(null, list[4]);
     else {
-        setTimeout(function () {
+        setInterval(function () {
             callback(null, list[5]);
-        }, 5000);
-        
+        }, 1000);
+
     }
 }
 console.log("passed here 0");
@@ -31,6 +35,45 @@ maslahatBering(65, (err, data) => {
     }
 });
 console.log("passed here 1");
+
+// Async function maslahatBering (a) {
+//     if (typeof a !== 'number') throw new Error("insert a number");
+//     else if (a <= 20) return list[0];
+//     else if (a > 20 && a <= 30) return list[1];
+//     else if (a > 30 && a <= 40) return list[2];
+//     else if (a > 40 && a <= 50) return list[3];
+//     else if (a > 50 && a <= 60) return list[4];
+//     else {
+//         return new Promise((resolve, reject) => {
+//             setTimeout(() => {
+//                  resolve (list[5]);
+//             }, 5000);  
+//         });
+//     }
+// }
+// then/catch
+// console.log("passed here 0");
+// maslahatBering(65)
+// .then((data) => {
+//     console.log('javob', data);
+// })
+// .catch((err) => {
+//     console.log('Error', err);
+// });
+// console.log('passed here 1')
+
+// async/await
+// async function run() {
+//     let javob = await maslahatBering(65);
+//     console.log(javob);
+//     javob = await maslahatBering(31);
+//     console.log(javob);
+//     javob = await maslahatBering(41);
+//     console.log(javob);
+// }
+// run();
+
+
 
 
 
