b46fec6f8c64e38e77c58d248544717c6960e582
boburtojiboev <tbobur1995@gmail.com>
2023-09-29 18:23:24 +0900

    fix: refactoring

---
+++ test.js
@@ -0,0 +1,28 @@
+//  const fs = require("fs");
+//  let user;
+// fs.readFile("database/user.json", "utf8", (err, data) => {
+//     if(err) {
+//         console.log("ERROR:", err);
+//     }else {
+//         user = JSON.parse(data);
+//     }
+// });  
+ 
+// function fetchData() {
+//     return new Promise((resolve, reject) => {
+//       const data = user;
+//       const error = false;
+  
+//       if (error) {
+//         reject("someting went wrong!");
+//       } else {
+//         resolve(data);
+//       }
+//     });
+//   }
+  
+//   fetchData()
+//     .then((data) => console.log("data: ", data))
+//     .catch((err) => console.log("err", err));
+
+
