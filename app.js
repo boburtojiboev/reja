7101342d59d8ee6beacf7479f491f2782b1983d1
boburtojiboev <tbobur1995@gmail.com>
2023-09-29 20:13:00 +0900

    feat: mongoDB ulanish logic

---
+++ app.js
@@ -0,0 +1,50 @@
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+// const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    }else {
+        user = JSON.parse(data);
+    }
+});  
+
+// MongoDB chaqirish
+const db = require("./server").db();
+
+// 1 Kirish code lar
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+// 2: session code 
+ 
+// 3 views code
+app.set("views", "views");
+app.set("view engine", "ejs");  
+
+
+// 4 Routing code
+app.post('/create-item', (req, res) => {
+    console.log(req.body);
+    res.json({test: "success" }); 
+});
+ 
+app.get("/author", (req, res) =>{
+    res.render("author", {user: user});
+})
+
+app.get('/', function(req, res) {
+    res.render("reja");
+}); 
+module.exports = app;
+
+// const server = http.createServer(app);
+// let PORT = 3000;
+// server.listen(PORT, function (){
+//     console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`)
+// });
--- server.js
+++ server.js
@@ -1,46 +1,27 @@
-console.log("Web Serverni boshlash");
-const express = require("express");
-const app = express();
 const http = require("http");
-const fs = require("fs");
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-    if(err) {
-        console.log("ERROR:", err);
-    }else {
-        user = JSON.parse(data);
-    }
-});  
+const mongodb = require("mongodb");
 
-// 1 Kirish code lar
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+let db;
+const connectionString = "mongodb+srv://tbobur1995:t27011995rBobur@cluster0.5lxzxir.mongodb.net/Reja";
+mongodb.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 
-// 2: session code 
- 
-// 3 views code
-app.set("views", "views");
-app.set("view engine", "ejs");  
-
-
-// 4 Routing code
-app.post('/create-item', (req, res) => {
-    console.log(req.body);
-    res.json({test: "success" }); 
-});
- 
-app.get("/author", (req, res) =>{
-    res.render("author", {user: user});
-})
-
-app.get('/', function(req, res) {
-    res.render("reja");
-}); 
-
-const server = http.createServer(app);
+}, (err, client) => {
+    if(err) console.log("ERROR on connection MongoDB")
+    else {
+        console.log("MongoDB connection succeed");
+        module.exports = client;
+        
+        const app = require("./app");
+        const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function (){
-    console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`)
+    console.log(`The server is running successfully on port: ${PORT}, http://localhost:${PORT}`);
 });
+}
+});
+
+
+
