518fa27df943233a28bd7ef64a745e1f3c8c7792
boburtojiboev <tbobur1995@gmail.com>
2023-09-29 20:21:29 +0900

    fix: modifiing TASK-E

--- train.js
+++ train.js
@@ -1,40 +1,53 @@
+// task-E
+//  Shunday function tuzing, u bitta string argumentni qabul qilib osh stringni teskari qilib return qilsin.
+// masalan: getReverse("hello") return qilsin "olleh"
+
+function getReverse(str) {
+    let reverse = str.split('').reverse().join('');
+    console.log(reverse);
+  }
+  getReverse("WEB_DEVELOPMENT");
+
+
+
+
 // Async functionlarni qo'llash
 
-console.log("Jack Ma maslahatlari");
-const list = [
-    "yaxshi talaba boling", //0=20
-    "togri boshliqning tanlang va koproq xato qiling", //20-30
-    "o'zingizga ishlashingizni boshlang", //30-40
-    "siz kuchli bolgan narsalarni qiling", //40-50
-    "yoshlarga investitsiya qiling", //50-60
-    "endi dam oling, foydasi yoq" //60
-];
+// console.log("Jack Ma maslahatlari");
+// const list = [
+//     "yaxshi talaba boling", //0=20
+//     "togri boshliqning tanlang va koproq xato qiling", //20-30
+//     "o'zingizga ishlashingizni boshlang", //30-40
+//     "siz kuchli bolgan narsalarni qiling", //40-50
+//     "yoshlarga investitsiya qiling", //50-60
+//     "endi dam oling, foydasi yoq" //60
+// ];
 
 
-// Nodejs event loop va Callback functionlarni organamiz
+// // Nodejs event loop va Callback functionlarni organamiz
 
-function maslahatBering (a, callback) {
-    if (typeof a !== 'number') callback("insert a number", null);
-    else if (a <= 20) callback(null, list[0]);
-    else if (a > 20 && a <= 30) callback(null, list[1]);
-    else if (a > 30 && a <= 40) callback(null, list[2]);
-    else if (a > 40 && a <= 50) callback(null, list[3]);
-    else if (a > 50 && a <= 60) callback(null, list[4]);
-    else {
-        setInterval(function () {
-            callback(null, list[5]);
-        }, 1000);
+// function maslahatBering (a, callback) {
+//     if (typeof a !== 'number') callback("insert a number", null);
+//     else if (a <= 20) callback(null, list[0]);
+//     else if (a > 20 && a <= 30) callback(null, list[1]);
+//     else if (a > 30 && a <= 40) callback(null, list[2]);
+//     else if (a > 40 && a <= 50) callback(null, list[3]);
+//     else if (a > 50 && a <= 60) callback(null, list[4]);
+//     else {
+//         setInterval(function () {
+//             callback(null, list[5]);
+//         }, 1000);
 
-    }
-}
-console.log("passed here 0");
-maslahatBering(65, (err, data) => {
-    if (err) console.log("error", err);
-    else {
-        console.log('javob', data);
-    }
-});
-console.log("passed here 1");
+//     }
+// }
+// console.log("passed here 0");
+// maslahatBering(65, (err, data) => {
+//     if (err) console.log("error", err);
+//     else {
+//         console.log('javob', data);
+//     }
+// });
+// console.log("passed here 1");
 
 // Async function maslahatBering (a) {
 //     if (typeof a !== 'number') throw new Error("insert a number");
