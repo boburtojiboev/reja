f7fb150ccce675ba07ce638f5ffa10e4fc930eba
boburtojiboev <tbobur1995@gmail.com>
2023-10-05 00:33:43 +0900

    feat: Reja loyihamizning yakuniy bosqichi

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ fs.readFile("database/user.json", "utf8", (err, data) => {
 
 // MongoDB chaqirish
 const db = require("./server").db();
+const mongodb = require("mongodb");
 
 // 1 Kirish code lar
 app.use(express.static("public"));
@@ -45,6 +46,38 @@ app.post('/create-item', (req, res) => {
         res.json(data.ops[0]);  
     });
 }); 
+
+app.post("/delete-item", (req, res) => {
+    const id = req.body.id;
+    db.collection("plans").deleteOne({_id: new mongodb.ObjectId(id)}, function(err, data) {
+        res.json({state: "success"});
+    })
+});
+
+app.post("/edit-item", (req, res) => {
+    const data = req.body;
+    console.log(data);
+    db.collection("plans").findOneAndUpdate(
+        {_id: new mongodb.ObjectId(data.id)}, 
+        {$set: {reja: data.new_input}},
+        function (err, data) {
+           res.json({state: "success"}); 
+        });
+});
+
+app.post("/delete-all", (req, res) => {
+    if (req.body.delete_all) {
+      db.collection("plans").deleteMany(function() {
+        res.json({state: "hamma rejalar ochirildi"});
+
+      });
+    }
+});
+
+
+
+
+
  
 app.get("/author", (req, res) =>{ 
     res.render("author", {user: user});
@@ -66,4 +99,4 @@ app.get('/', function(req, res) {
 module.exports = app;  
 
 
-
+ 
--- public/browser.js
+++ public/browser.js
@@ -29,3 +29,50 @@ document.getElementById("create-form").addEventListener("submit", function (e) {
         console.log("Iltimos qaytadan harakat qiling!");
     });
 });  
+
+
+document.addEventListener("click", function (e) {
+  // delete operation
+  console.log(e.target);
+  if (e.target.classList.contains("delete-me")) {
+    if (confirm("aniq ochirmoqchimisiz?")) {
+      axios.post("/delete-item", {id: e.target.getAttribute("data-id")})
+      .then((response) => {
+        console.log(response.data);
+        e.target.parentElement.parentElement.remove();
+      })
+      .catch((err) => {
+       console.log("Iltimos qaytadan harakat qiling!");
+      });
+    }
+  }
+
+
+
+// edit operation
+if (e.target.classList.contains("edit-me")) {
+  let userInput = prompt("Ozgartirish kiriting", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML
+  );
+  if (userInput) {
+    axios.post("/edit-item", {
+    id: e.target.getAttribute("data-id"), 
+    new_input: userInput,
+  }).then(response => {
+    console.log(response);
+    e.target.parentElement.parentElement.querySelector(".item-text")
+    .innerHTML = userInput;
+
+  })
+  .catch(err => {
+    console.log("Iltimos qaytadan harakat qiling!");
+  });
+  }
+}
+});
+
+document.getElementById("clean-all").addEventListener("click", function () {
+  axios.post("/delete-all", {delete_all: true}).then(response => {
+    alert(response.data.state);
+    document.location.reload();
+  });
+});
